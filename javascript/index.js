ed839c3f81841a2bd26e65f2c7294c2a6ef2e468
PatriciaFig123 <patriciafigueiredo20@icloud.com>
2026-01-05 23:38:02 +0000

    changed the time

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,22 @@
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone.length > 0) {
-    let cityTime = moment().tz(cityTimeZone);
-    let formattedTime = cityTime.format("dddd, MMMM D, YYYY h:mm A");
-    alert(`It is ${formattedTime} in ${cityTimeZone}`);
-  }
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY ");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY ");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
-let citySelect = document.querySelector("#city-selector");
-citySelect.addEventListener("change", updateCity);
+updateTime;
+setInterval(updateTime, 1000);
