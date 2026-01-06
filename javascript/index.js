842f46fc7adc137353aa88964c45f36592f1fd33
PatriciaFig123 <patriciafigueiredo20@icloud.com>
2026-01-06 00:52:39 +0000

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -16,13 +16,14 @@ function updateTime() {
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY ");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
-  function updateCity(event) {
-    let cityTimeZone = event.target.value;
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `<div class="city">
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
         <div>
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY ")}</div>
@@ -32,7 +33,6 @@ function updateTime() {
         )} <small>${cityTime.format("A")}</small>
         </div>
       </div>`;
-  }
 }
 
 updateTime();
